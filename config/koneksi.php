38f89560dd55279ef1c68489d21520b1054488d2
megacandravinata <megasmkn12@gmail.com>
2025-02-17 14:02:44 +0700

    init

---
+++ config/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = 'localhost';
+$userdb = 'root';
+$passdb = '';
+$namedb = 'UKK2025_XIIPPLG2_7728';
+
+$koneksi = mysqli_connect($hostname,$userdb,$passdb,$namedb);
+
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+    <title>To Do List</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">To Do List</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" arial-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+            <div class="navbar-nav me-auto">
+
+            </div>
+            <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+            <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+            </div>
+        </div>
+</nav>
+
+<footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
+    <p>&copy; To Do List | Mega Candra Vinata</p>
+</footer>
+<script type="text/javascript" src="asset/js/bootstrap.min.js"></script>
+</body>
+</html>
